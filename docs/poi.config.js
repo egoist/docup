f7edd87887c8c20482eec3c48f8f13f998ad3a7e
EGOIST <0x142857@gmail.com>
2020-05-16 22:35:08 +0800

    fix build config for website

--- docs/poi.config.js
+++ docs/poi.config.js
@@ -5,31 +5,32 @@ const pkg = require('../package')
 module.exports = {
   entry: ['docs/main.js'],
   output: {
-    dir: 'docs/dist'
+    dir: 'docs/dist',
+    html: {
+      title: 'Docup',
+    },
   },
-  html: {
-    title: 'Docup'
-  },
+  publicFolder: 'docs',
   envs: {
     DOCUP_VERSION: pkg.version,
     PRISM_VERSION: require('prismjs/package').version,
-    PRISM_LANGUAGES: getPrismLanguages()
+    PRISM_LANGUAGES: getPrismLanguages(),
   },
   babel: {
-    jsx: 'preact'
+    jsx: 'preact',
   },
   devServer: {
     after(app) {
       app.use(express.static('docs'))
-    }
-  }
+    },
+  },
 }
 
 function getPrismLanguages() {
   const files = fs.readdirSync('node_modules/prismjs/components')
   return files
-    .filter(file => file.endsWith('.min.js'))
-    .map(file => {
+    .filter((file) => file.endsWith('.min.js'))
+    .map((file) => {
       const [, name] = /\-([^\.]+)/.exec(file)
       return name
     })
