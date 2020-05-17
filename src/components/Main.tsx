4cb89ee985ecb49a1e6bf56ce39c84857612c4df
EGOIST <0x142857@gmail.com>
2020-05-17 17:04:40 +0800

    fix(style): make content wider on large screen

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -4,7 +4,7 @@ export const Main: FunctionComponent<{ html: string }> = ({ html }) => {
   return (
     <div class="mt-12 main">
       <div
-        class="max-w-2xl content"
+        class="max-w-2xl xl:max-w-4xl content"
         dangerouslySetInnerHTML={{ __html: html }}
       ></div>
     </div>
