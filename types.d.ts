86254df9f23fbdaae9130a2b9dc0f7d92baf41ce
EGOIST <0x142857@gmail.com>
2021-03-26 13:25:21 +0800

    chore: switch to github actions

--- types.d.ts
+++ types.d.ts
@@ -1,10 +1,3 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    DOCUP_VERSION: string
-    PRISM_VERSION: string
-  }
-}
-
 declare const DOCUP_VERSION: string
 declare const PRISM_VERSION: string
 declare const PRISM_LANGUAGES: string
